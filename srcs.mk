db83b45329111cfa62ae184e2a21fa7e2963ecb3
Pablo <pablolamtenzan@gmail.com>
2022-04-12 21:00:22 +0200

    Update makefile's target files

--- srcs.mk
+++ srcs.mk
@@ -3,14 +3,33 @@ SRCDIR	=	srcs
 
 HDRS	=\
 $(addprefix includes/,\
+	ftlibc.h\
 	wd_crypt.h\
+	wd_error.h\
+	wd_parse.h\
 	wd_types.h\
+	wd_utils.h\
+	woody_woodpacker.h\
 )
 SRCS	=\
 $(addprefix srcs/,\
+	build_decryptor.c\
 	$(addprefix crypt/,\
 		kcrypt.c\
 		kdecrypt.c\
 	)\
+	encrypt_chunks.c\
+	$(addprefix ftlibc/,\
+		memcpy.c\
+		strcmp.c\
+		strlen.c\
+		strncmp.c\
+	)\
+	genkey.c\
+	inject_decryptor.c\
+	lookup_sections.c\
 	main.c\
+	map_elf.c\
+	parse_opts.c\
+	test.c\
 )
